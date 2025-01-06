07db2d10cef0fdb38a216c49f6a10ebe421eb907
gyubo <yoon9yubo@gmail.com>
2025-01-06 21:25:41 +0900

    0106 useState, useRef !!!!

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,57 @@
+import {useState} from 'react';
+
 import NewProject from "./components/NewProjects.jsx";
+import NoProjectSelected from "./components/NoProjectSelected.jsx";
 import ProjectsStidebar from "./components/ProjectsSidebar.jsx";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProject: undefined,
+    projects: []
+  });
+
+  function handleStartAddProject() {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProject: null,
+      };
+    });
+  }
+
+  function handleAddProject(projectData) {
+    setProjectsState(prevState => {
+      const newProject = {
+        ...projectData,
+        id: Math.random()
+      }
+
+      return {
+        ...prevState,
+        selectedProject: undefined,
+        projects: [...prevState.projects, newProject ]
+      }
+    })
+  }
+
+  console.log(projectsState);
+
+  let content;
+
+  if(projectsState.selectedProject === null) {
+    content = <NewProject onAdd={handleAddProject}/>
+  } else if(projectsState.selectedProject === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />
+  }
+  
   return (
-    <>
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsStidebar />
-      <NewProject />
+      <ProjectsStidebar onStartAddProject={handleStartAddProject} projects={projectsState.projects}/>
+      {/* <NewProject /> */}
+      {/* <NoProjectSelected onStartAddProject={handleStartAddProject} /> */}
+      {content}
     </main>
-    </>
+    
   );
 }
 
